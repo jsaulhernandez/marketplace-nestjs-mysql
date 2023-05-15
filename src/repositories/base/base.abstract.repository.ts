40bb1cecd902f2bfd42d3c1fef77bcc1e8664cad
Sony <jsaulhernandez98@gmail.com>
2023-05-14 18:24:23 -0600

    tag: built base repository

---
+++ src/repositories/base/base.abstract.repository.ts
@@ -0,0 +1,35 @@
+import { DeleteResult, FindOptionsWhere, Repository } from 'typeorm';
+
+import { BaseInterfaceRepository } from './base.interface.repository';
+
+export abstract class BaseAbstractRepository<T> implements BaseInterfaceRepository<T> {
+    private entity: Repository<T>;
+
+    protected constructor(entity: Repository<T>) {
+        this.entity = entity;
+    }
+
+    public async create(data: T): Promise<T> {
+        return await this.entity.save(data);
+    }
+
+    public async findOneById(id: number): Promise<T> {
+        return await this.entity.findOneBy({ id: id } as unknown as FindOptionsWhere<T>);
+    }
+
+    public async findByCondition(filterCondition: any): Promise<T> {
+        return await this.entity.findOne({ where: filterCondition });
+    }
+
+    public async findWithRelations(relations: any): Promise<T[]> {
+        return await this.entity.find(relations);
+    }
+
+    public async findAll(): Promise<T[]> {
+        return await this.entity.find();
+    }
+
+    public async remove(id: number): Promise<DeleteResult> {
+        return await this.entity.delete(id);
+    }
+}
