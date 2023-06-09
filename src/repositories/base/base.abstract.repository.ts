75df4c611a898a8b4016e8808187b74ed934ff07
Sony <jsaulhernandez98@gmail.com>
2023-06-09 12:02:30 -0600

    tag: updating methods in base repository

--- src/repositories/base/base.abstract.repository.ts
+++ src/repositories/base/base.abstract.repository.ts
@@ -1,4 +1,4 @@
-import { DeleteResult, Repository } from 'typeorm';
+import { DeleteResult, FindManyOptions, FindOptionsWhere, Repository } from 'typeorm';
 
 import { BaseInterfaceRepository } from './base.interface.repository';
 
@@ -9,25 +9,31 @@ export abstract class BaseAbstractRepository<T> implements BaseInterfaceReposito
         this.entity = entity;
     }
 
-    public async save(data: T | any): Promise<T> {
+    public async save(data: T): Promise<T> {
         return await this.entity.save(data);
     }
 
-    public async findOne(conditions: any): Promise<T> {
+    public async findOne(
+        conditions: FindOptionsWhere<T>[] | FindOptionsWhere<T>,
+    ): Promise<T> {
         return await this.entity.findOne({
             where: conditions,
         });
     }
 
-    public async findOneBy(conditions: any): Promise<T> {
+    public async findOneBy(
+        conditions: FindOptionsWhere<T> | FindOptionsWhere<T>[],
+    ): Promise<T> {
         return await this.entity.findOneBy(conditions);
     }
 
-    public async findByCondition(filterCondition: any): Promise<T[]> {
+    public async findByCondition(
+        filterCondition: FindOptionsWhere<T> | FindOptionsWhere<T>[],
+    ): Promise<T[]> {
         return await this.entity.findBy(filterCondition);
     }
 
-    public async findWithRelations(relations: any): Promise<T[]> {
+    public async findWithRelations(relations: FindManyOptions<T>): Promise<T[]> {
         return await this.entity.find(relations);
     }
 
